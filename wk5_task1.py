f9a3fdfc9062a29ffae6e1807003d497862ad242
peter_wachira <peterwchr@gmail.com>
2017-12-12 13:21:55 -0500

    wk5 task 1 improved

--- wk5_task1.py
+++ wk5_task1.py
@@ -1,25 +1,26 @@
 from pyspark import SparkConf, SparkContext
 from operator import add
+import re
+
 sc = SparkContext(conf=SparkConf().setAppName("MyApp").setMaster("local"))
 
-import re
 
 def parse_article(line):
     try:
         article_id, text = unicode(line.rstrip()).split('\t', 1)
         text = re.sub("^\W+|\W+$", "", text, flags=re.UNICODE)
         words = re.split("\W*\s+\W*", text, flags=re.UNICODE)
-        bigram = []
-        for idx, word in enumerate(words):
-            if word.lower() == 'narodnaya' and idx < len(words) - 1 :
-                bigram.append(word.lower() + '_' + words[idx+1].lower())
-        return bigram 
+        bigram = [(words[i].lower(),words[i+1].lower()) for i in range(
+               0,len(words) - 1) if words[i].lower() == 'narodnaya']
+        return bigram
     except ValueError as e:
         return []
 
-wiki = sc.textFile("/data/wiki/en_articles_part/articles-part", 16).map(
-     parse_article).filter(
-        lambda a:len(a) > 0).flatMap(
-            lambda a:a).map(lambda w:(w,1)).reduceByKey(add).sortByKey()
+wiki = sc.textFile(
+           "/data/wiki/en_articles_part/articles-part", 16).flatMap(
+           parse_article).map(
+           lambda w:(w,1)).reduceByKey(
+           add).sortByKey()
+
 for name, num in wiki.collect():
     print '%s\t%d' % (name, num)
